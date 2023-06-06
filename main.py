728bd9957f0318e59c79c159b92774be7b6795f6
Murat Yilmaz <135061655+muratyilmaz729@users.noreply.github.com>
2023-06-06 10:17:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 10:14:54 2023
+
+@author: johan
+"""
+# This entrypoint file to be used in development. Start by reading README.md
+import demographic_data_analyzer
+
+
+# Test your function by calling it here
+demographic_data_analyzer.calculate_demographic_data()
